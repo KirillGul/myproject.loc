377de74807ab54cf1bdff343713e7875c47e8720
KirillGul <guliev.kirill@yandex.ru>
2022-02-08 17:51:23 +0300

    Работа

--- www/index.php
+++ www/index.php
@@ -1,10 +1,13 @@
 <?php
+
 /*
 require __DIR__ . '/../src/MyProject/Models/Users/User.php';
 require __DIR__ . '/../src/MyProject/Models/Articles/Article.php';
 */
 
-spl_autoload_register( static function (string $class) {
+use MyProject\Controllers\MainController;
+
+spl_autoload_register(static function (string $class) {
     $class = str_replace('\\', '/', $class); //В операционной системе Windows используются обратные слеши, а в операционной системе Linux используются прямые слеши. И операционная система Windows прекрасно понимает оба варианта, но для Linux важны прямые слеши. Если код запустить на Linux, на котором работают большинство серверов, то у нас ничего работать не будет. Напишем функцию, мы ищем обратные слеши и заменяем их на прямые
 
     $file = __DIR__ . '/../src/' .$class. '.php'; //Можем посмотреть путь если перед переменной $file добавить echo. Имя класса преобразуется в путь.
@@ -15,6 +18,20 @@ spl_autoload_register( static function (string $class) {
     }
 });
 
+/*
 $author = new \MyProject\Models\Users\User('Иван');
 $article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
 var_dump($article);
+*/
+/*
+$controller = new MainController;
+if (!empty($_GET['name'])) {
+    $controller->sayHello($_GET['name']);    
+} else {
+    $controller->main();
+}
+*/
+
+$route = $_GET['route'] ?? '';
+$routes = require __DIR__ . '/../src/routes.php';
+var_dump($routes);
