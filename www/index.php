4c8090aed9ec110ce0573c36994ed91d74d924d2
KirillGul <50946729+KirillGul@users.noreply.github.com>
2022-02-08 22:47:08 +0300

    Дом

--- www/index.php
+++ www/index.php
@@ -34,4 +34,25 @@ if (!empty($_GET['name'])) {
 
 $route = $_GET['route'] ?? '';
 $routes = require __DIR__ . '/../src/routes.php';
-var_dump($routes);
+
+$isRouteFound = false;
+foreach ($routes as $pattern => $controllerAndAction) {
+    preg_match($pattern, $route, $matches);
+    if (!empty($matches)) {
+        $isRouteFound = true;
+        break;
+    }
+}
+
+if (!$isRouteFound) {
+    echo 'Страница не найдена!';
+    return;
+}
+
+unset($matches[0]);
+
+$controllerName = $controllerAndAction[0];
+$actionName = $controllerAndAction[1];
+
+$controller = new $controllerName();
+$controller->$actionName(...$matches);
