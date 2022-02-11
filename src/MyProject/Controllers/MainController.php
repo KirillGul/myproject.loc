94b4cde24dd1e15b91b5bdd4d6c7204fde167fc8
KirillGul <50946729+KirillGul@users.noreply.github.com>
2022-02-11 17:59:30 +0300

    Дом

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -20,28 +20,15 @@ class MainController
     }
 
     public function main(): void
-    {
-        /*
-        $articles = [
-            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
-        ];
-        */
-        
+    {        
         $articles = $this->db->query('SELECT * FROM `articles`;');
-        var_dump($articles);
 
-        //$this->view->renderHtml('main/main.php', ['articles' => $articles]);
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
 
     public function sayHello(string $name): void
     {
 
         $this->view->renderHtml('main/hello.php', ['name' => $name, 'title' => 'Страница преветсвия']);
-    }
-
-    public function sayBye(string $name): void
-    {
-        echo "Пока, ". $name;
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,5 @@
 
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
-    '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
