3bc725ee9bdcfda79795cd10f636974ee1457f5f
KirillGul <guliev.kirill@yandex.ru>
2022-02-17 16:43:00 +0300

    Front-Controller

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
-    '~^articles/(\d+)/edit$~' => [\MyProject\Controllers\ArticlesController::class, 'edit'],
-    '~^articles/add$~' => [\MyProject\Controllers\ArticlesController::class, 'add'],
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
 ];
--- www/index.php
+++ www/index.php
@@ -4,10 +4,27 @@ spl_autoload_register(function (string $className) {
     require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
 });
 
-$controller = new \MyProject\Controllers\MainController();
+$route = $_GET['route'] ?? '';
+$routes = include_once __DIR__ . '/../src/routes.php';
 
-if (!empty($_GET['name'])) {
-    $controller->sayHello($_GET['name']);
-} else {
-    $controller->main();
+$isRouteFound = false;
+foreach ($routes as $pattern => $controllerAndAction) {
+    preg_match($pattern, $route, $matches); //будет не пустое даже без аргументов, когда найдет общее совпадение
+    if (!empty($matches)) { //и в $matches сохраняються аргументы
+        $isRouteFound = true;
+        break;
+    }  
 }
+
+if (!$isRouteFound) {
+    echo 'Страница не найдена!';
+    return;
+}
+
+unset($matches[0]);
+
+$controllerName = $controllerAndAction[0];
+$actionName = $controllerAndAction[1];
+
+$controller = new $controllerName();
+$controller->$actionName(...$matches);
